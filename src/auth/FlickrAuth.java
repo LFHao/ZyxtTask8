12be999a8007384f5fb2fcdbb77641b0291bf563
unknown <Yash@yash-pc.com>
2014-02-11 14:45:13 -0500

    flickrapi package added

---
+++ src/auth/FlickrAuth.java
@@ -0,0 +1,6 @@
+package auth;
+
+public class FlickrAuth {
+	public static String KEY = "e673e2be6f030f34735dfe4b7b444306";
+	public static String SERVER = "www.flickr.com";
+}
---
+++ src/flickrapi/FlickrSearchTopic.java
@@ -0,0 +1,8 @@
+package flickrapi;
+
+import auth.*;
+import databeans.*;
+
+public class FlickrSearchTopic {
+
+}
