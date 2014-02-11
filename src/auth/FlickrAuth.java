bcfc2caa3e6d44653b18e906552d2f88141df75d
unknown <Yash@yash-pc.com>
2014-02-11 16:16:31 -0500

    flickr added

--- src/auth/FlickrAuth.java
+++ src/auth/FlickrAuth.java
@@ -3,4 +3,8 @@ package auth;
 public class FlickrAuth {
 	public static String KEY = "e673e2be6f030f34735dfe4b7b444306";
 	public static String SERVER = "www.flickr.com";
+	
+	public String getKey(){
+		return KEY;
+	}
 }
--- src/controller/MapAction.java
+++ src/controller/MapAction.java
@@ -1,8 +1,16 @@
 package controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import com.aetrion.flickr.FlickrException;
+import com.aetrion.*;
 
 import databeans.*;
 import flickrapi.*;
@@ -22,6 +30,15 @@ public class MapAction extends Action {
 		
 		String location = request.getParameter("location");
 		ArrayList<Tweet> tweets = TwitterSearchTopic.searchTopic(location, 50);
+		try {
+			System.out.println("WTF");
+			List<String> photos = FlickrSearchTopic.getPhotos(location);
+			for (String url : photos) {
+				System.out.println(url);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		System.out.println(tweets.size() + " tweets returned.");
 		
 		request.setAttribute("tweets", tweets);
